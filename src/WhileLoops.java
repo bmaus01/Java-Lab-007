300d74787cf59066cec20a0f2d9be0d05b897a49
bryan <bmaus719@mycr.redwoods.edu>
2023-03-09 22:21:56 -0800

    Part 1 completed

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,14 +1,43 @@
 import java.util.Scanner;
-
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
-
     public static void eoCount() {
-    }
+        int number;
+        int countOne=0;
+        int countTwo=0;
+        System.out.println("\nThis program will determine the number of odd and even number you have entered.\n");
+        do {
+            System.out.print("Enter the number 0 to exit, otherwise enter a random number: ");
+            number = s.nextInt();
+            if (number != 0) {
+                if (number % 2 == 0) {
+                    countOne++;
+                } else {
+                    countTwo++;
+                }
+            }
+        }
+        while (number != 0);
 
+        System.out.print("\nThere are " + countOne + " even number(s) ");
+        System.out.println("and " + countTwo + " odd number(s).\n\n");
+    }
     public static void mean() {
+    int number;
+    int countOne = 0;
+    int countTwo = 0;
+        System.out.println("This program will determine the Mean of the numbers you enter.\n");
+        do {
+            System.out.print("Enter the number 0 to exit, otherwise enter a random number: ");
+            number = s.nextInt();
+            if (number != 0) {
+                countOne = countOne + number;
+                countTwo++;
+                }
+            }
+        while (number != 0);
+        System.out.println("\nThe Mean of the " + countTwo + " numbers is: " + (countOne/countTwo));
     }
-
     public static void main(String[] args) {
         eoCount();
         mean();
